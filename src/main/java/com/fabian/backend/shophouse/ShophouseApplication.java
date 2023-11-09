ebf6a352312aaad749be6ada6607b4d5990a7a73
fabiancarvajal2023 <wilson.carvajal@arandasoft.com>
2023-11-09 08:00:37 -0500

    firts commit

---
+++ src/main/java/com/fabian/backend/shophouse/ShophouseApplication.java
@@ -0,0 +1,13 @@
+package com.fabian.backend.shophouse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShophouseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShophouseApplication.class, args);
+	}
+
+}
